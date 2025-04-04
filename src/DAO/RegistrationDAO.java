25226f5114077ee0cfb952fb3663aefd907345e4
Phong <phongnd.b21vt342@stu.ptit.edu.vn>
2025-04-04 22:11:44 +0700

    Update RegistrationDAO

--- src/DAO/RegistrationDAO.java
+++ src/DAO/RegistrationDAO.java
@@ -1,5 +1,40 @@
 package DAO;
 
-public class RegistrationDAO {
+import Model.Registration;
+import connectDatabase.DatabaseConnect;
 
+import java.awt.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.util.*;
+public class RegistrationDAO {
+    public void add(int student_id, int subject_id){
+        String sql = "INSERT INTO registrations (student_id,subject_id) VALUES (?,?)";
+        try {
+            Connection cn = DatabaseConnect.getConnection();
+            PreparedStatement pr = cn.prepareStatement(sql)
+            pr.setInt(1,student_id);
+            pr.setInt(2,subject_id);
+            System.out.println("Successfully");
+        } catch (Exception e) {
+            System.out.println("Error Add");
+        }
+    }
+    public void remove(int student_id, int subject_id){
+        String sql = "DELETE FROM registrations WHERE subject_id = ? AND student_id = ?";
+        try {
+            Connection cn = DatabaseConnect.getConnection();
+            PreparedStatement pr = cn.prepareStatement(sql);
+            pr.setInt(1,student_id);
+            pr.setInt(2,subject_id);
+            pr.executeUpdate();
+            System.out.println("Remove Success");
+        } catch(Exception e){
+            System.out.println("Error Remove");
+        }
+    }
+    public void showliststudent(){
+
+    }
 }
+
