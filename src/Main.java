1d0b30bc1a67d6382ec64a19004b1fdf338466a7
Khoi-git <khoiduy03hd@gmail.com>
2025-04-14 09:10:13 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -8,8 +8,8 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         System.out.println("====== STUDENT MANAGEMENT SYSTEM ======");
         System.out.println("1. Quản lý kí túc xá");
-        System.out.println("2. Báo cáo thống kê");
-        System.out.println("3. Quản lý môn học");
+        System.out.println("2. Quản lý môn học");
+        System.out.println("3. Hỗ trợ sinh viên");
         System.out.println("=================================");
         System.out.print("Chọn chức năng: ");
 
@@ -18,10 +18,12 @@ public class Main {
             DormitoryView.main(null);
         }
         else if (i == 2) {
-            ReportView.main(null);
-        } else if (i == 3) {
             SubjectView.main(null);
-        }  else {
+        }
+        else if (i == 3) {
+            SupportRequestView.main(null);
+        }
+        else {
             System.out.println("Lựa chọn không hợp lệ!");
         }
     }
