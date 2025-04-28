d7b06c9042d51664cd78c97449388b1b04bfd431
Phong <phongnd.b21vt342@stu.ptit.edu.vn>
2025-04-28 23:17:47 +0700

    Update 28/4 detail

--- src/Main.java
+++ src/Main.java
@@ -5,17 +5,31 @@ import View.ViewRegister;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int i = Integer.parseInt(sc.nextLine());
-        if(i==1){
-            ViewCalenda.main(null);
-        }
-        else if(i==2){
-            ViewGrade.main(null);
-        }
-        else if(i==3){
-            ViewRegister.main(null);
+
+        while (true) {
+            System.out.println("=== MENU CHÍNH ===");
+            System.out.println("1. Quản lý lịch học");
+            System.out.println("2. Quản lý điểm");
+            System.out.println("3. Quản lý đăng ký");
+            System.out.println("0. Thoát chương trình");
+            System.out.print("Chọn chức năng: ");
+
+            int i = Integer.parseInt(sc.nextLine());
+
+            if (i == 1) {
+                ViewCalenda.main(null);
+            } else if (i == 2) {
+                ViewGrade.main(null);
+            } else if (i == 3) {
+                ViewRegister.main(null);
+            } else if (i == 0) {
+                System.out.println("Đăng xuất");
+                break;
+            } else {
+                System.out.println("Lựa chọn không hợp lệ. Vui lòng chọn lại.");
+            }
         }
     }
 }
